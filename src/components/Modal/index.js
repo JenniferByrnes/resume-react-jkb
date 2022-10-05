0118d6d51916515a7eb213553c1438a8dc324e16
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-05 19:00:30 -0400

    removed large picture reference

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,7 +7,7 @@ function Modal({ onClose, currentPhoto }) {
     <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">{name}</h3>
-        <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+        <img src={require(`../../assets/projects/${index}.jpg`)} alt="current category" />
         <p>{description}</p>
           <button onClick={onClose} type="button">Close this modal</button>
         </div>
