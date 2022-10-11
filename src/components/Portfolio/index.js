82817c8c0780a1941d3bdd32ac9a3e150bc27a39
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-11 00:37:23 -0400

    fixed portfolio cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,19 +9,21 @@ const currentPhotos = photos;
 function Portfolio() {
 
   return (
-    <Container>
+    <Container className="flex">
       <h1>My Projects</h1>
       <Row className="flex-row">
         {currentPhotos.map((image, i) => (
-          <Col sm="6">
-            <Card body>
+          <Col xs="12" md="6" xl="4">
+            <CardBody style={{ minWidth: "350px", height: "540px" }}>
               <CardImg
                 src={require(`../../assets/projects/${i}.png`)}
                 alt={image.name}
                 key={image.name}
+                style={{ minWidth: "350px", height: "500px" }}
+                width="100%"
                 onClick={() => ProjectDetail(i)}
               />
-            </Card>
+            </CardBody>
           </Col>
         )
         )}
