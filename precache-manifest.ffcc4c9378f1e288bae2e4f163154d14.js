304312b9028a86981304f91ab377853ccb68f70c
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-10-13 15:00:13 -0400

    Updates

---
+++ precache-manifest.ffcc4c9378f1e288bae2e4f163154d14.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9610b04c732aa67b989b",
+    "url": "/react-resume-jkb/static/css/main.520fc289.chunk.css"
+  },
+  {
+    "revision": "9610b04c732aa67b989b",
+    "url": "/react-resume-jkb/static/js/main.9610b04c.chunk.js"
+  },
+  {
+    "revision": "877ddeaf8032078f6663",
+    "url": "/react-resume-jkb/static/js/1.877ddeaf.chunk.js"
+  },
+  {
+    "revision": "fb63b02dea7bf237bce5",
+    "url": "/react-resume-jkb/static/css/2.77d94960.chunk.css"
+  },
+  {
+    "revision": "fb63b02dea7bf237bce5",
+    "url": "/react-resume-jkb/static/js/2.fb63b02d.chunk.js"
+  },
+  {
+    "revision": "2d8a51f8d9e1cee5853a",
+    "url": "/react-resume-jkb/static/js/runtime~main.2d8a51f8.js"
+  },
+  {
+    "revision": "f40a363f89f1e667fc6f47b5304b5d2a",
+    "url": "/react-resume-jkb/static/media/JByrnesPhoto1.f40a363f.jpeg"
+  },
+  {
+    "revision": "fdc3d62e2dd1ed7cb4ff21e77589bddd",
+    "url": "/react-resume-jkb/static/media/Byrnesresume.fdc3d62e.pdf"
+  },
+  {
+    "revision": "c5e4a057355e0f6e2338cd247820d0c0",
+    "url": "/react-resume-jkb/static/media/0.c5e4a057.png"
+  },
+  {
+    "revision": "0067ecf7947248ebcc93fa1759ed61d8",
+    "url": "/react-resume-jkb/static/media/1.0067ecf7.png"
+  },
+  {
+    "revision": "0139d0383b75327000ddd58486b324f6",
+    "url": "/react-resume-jkb/static/media/2.0139d038.png"
+  },
+  {
+    "revision": "a9211ccf7dd64573b0abe6b08db42a55",
+    "url": "/react-resume-jkb/static/media/3.a9211ccf.png"
+  },
+  {
+    "revision": "9801dcc69a8c493ef0e37fac97274621",
+    "url": "/react-resume-jkb/static/media/4.9801dcc6.png"
+  },
+  {
+    "revision": "e3bb726164c57b7df9036596c41ee470",
+    "url": "/react-resume-jkb/static/media/5.e3bb7261.png"
+  },
+  {
+    "revision": "e6fc90cdb2942443eebe2810697358b9",
+    "url": "/react-resume-jkb/static/media/horiseon.e6fc90cd.png"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-resume-jkb/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-resume-jkb/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-resume-jkb/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-resume-jkb/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-resume-jkb/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-resume-jkb/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-resume-jkb/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-resume-jkb/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-resume-jkb/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-resume-jkb/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-resume-jkb/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-resume-jkb/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-resume-jkb/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-resume-jkb/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-resume-jkb/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-resume-jkb/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "feeae555d492e2459d9112e04e7c5a3e",
+    "url": "/react-resume-jkb/index.html"
+  }
+];
